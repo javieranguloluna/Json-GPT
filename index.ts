106fbcdeb7ca6c3b111298b5fa633117801d1f74
JAL <54935581+javieranguloluna@users.noreply.github.com>
2023-04-20 22:09:20 +0200

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,21 @@
+export {
+    solve,
+    SolveRequest,
+    SolveRequestOptions,
+    SolveResponse
+} from './solve'
+
+export {
+    solveJson,
+    SolveJsonRequest,
+    SolveJsonResponse
+} from './solve-json'
+
+export {
+    solveChat,
+    SolveChatRequest,
+    Conversation,
+    UserMessage,
+    AIMessage,
+    SystemMessage
+} from './solve-chat'
